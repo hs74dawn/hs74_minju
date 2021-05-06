99ab5949f391ef5d873d94c6e05fe6a605bba30d
hanvit <gksqlcWkd!>
2021-05-06 22:51:45 +0900

    test
    v1

---
+++ data.js
@@ -0,0 +1,42 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dsc02687-pano",
+      "name": "DSC02687-Pano",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1567,
+      "initialViewParameters": {
+        "yaw": 2.00776020845601,
+        "pitch": 0.1248736326964881,
+        "fov": 1.687004430252837
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
